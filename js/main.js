da1621e5a94c6023cbcb4a9f29f142154e14964d
Johanna Geßner <johanna-gessner@web.de>
2023-08-14 13:08:02 +0200

    iceberg image and acceleration implemented

--- js/main.js
+++ js/main.js
@@ -1,74 +1,104 @@
 class Game {
-    constructor () {
+    constructor() {
         this.ship = new Ship
         this.icebergsArr = [];
         this.numberOfIcebergsPassed = 0;
     }
+
     start() {
 
         // attach event listener
         this.attachEventListener();
 
+        // track acceleration state
+        this.isAccelerated = false;
+
         // make icebergs appear
         setInterval(() => {
-            const newIcebergs = new Icebergs();
+            const newIcebergs = new Icebergs(this.isAccelerated);
             this.icebergsArr.push(newIcebergs);
         }, 3000);
 
         // move icebergs to the left 
         setInterval(() => {
-            this.icebergsArr.forEach((iceberg)=> {
+            this.icebergsArr.forEach((iceberg) => {
                 iceberg.moveLeft(); // move
                 this.removeIcebergsIfOutside(iceberg); // remove if outside
                 this.detectCollision(iceberg); // detect collision
             })
         }, 100);
     }
+
     attachEventListener() {
         document.addEventListener("keydown", (event) => {
             if (event.key === "ArrowDown") {
                 this.ship.moveDown();
             } else if (event.key === "ArrowUp") {
                 this.ship.moveUp();
+            } else if (event.key === "ArrowRight") {
+                this.toggleAcceleration();
             }
         })
     }
-    removeIcebergsIfOutside (iceberg) {
-        if(iceberg.positionX < 0 - iceberg.size) {
+
+    toggleAcceleration() {
+        this.isAccelerated = true;
+        this.accelerateVisibleIcebergs();
+        
+        setTimeout(() => {
+            this.isAccelerated = false;
+        }, 6000);
+    }
+
+    accelerateVisibleIcebergs() {
+        this.icebergsArr.forEach((iceberg) => {
+            if (iceberg.positionX <= 100) {
+                iceberg.isAccelerated = true;
+            }
+        });
+
+        setTimeout(() => {
+            this.icebergsArr.forEach((iceberg) => {
+                if (iceberg.positionX <= 100) {
+                    iceberg.isAccelerated = false;
+                }
+            })
+        }, 6000);
+    }
+
+    removeIcebergsIfOutside(iceberg) {
+        if (iceberg.positionX < 0 - iceberg.size) {
             iceberg.domElement.remove(); // remove from the dom
             this.icebergsArr.shift(); // remove from the array
-
             game.icebergsPassed(); // counts passed icebergs
         }
     }
+
     icebergsPassed() {
         this.numberOfIcebergsPassed++;
-        if (this.numberOfIcebergsPassed >= 3) {
+        if (this.numberOfIcebergsPassed >= 10) {
+
+            // all icebergs passed
             location.href = "./winningpage.html";
         }
     }
 
     detectCollision(iceberg) {
-            const shipLeft = this.ship.positionX;
-            const shipRight = this.ship.positionX + this.ship.width;
-            const shipTop = this.ship.positionY;
-            const shipBottom = this.ship.positionY + this.ship.height;
-        
-            const icebergLeft = iceberg.positionX;
-            const icebergRight = iceberg.positionX + iceberg.size;
-            const icebergTop = iceberg.positionY;
-            const icebergBottom = iceberg.positionY + iceberg.size;
-        
-            if (
-                shipLeft < icebergRight &&
-                shipRight > icebergLeft &&
-                shipTop < icebergBottom &&
-                shipBottom > icebergTop
-                || // Oder-Bedingung hinzufügen für untere Kollision
-                shipTop < icebergBottom &&
-                shipBottom > icebergBottom &&
-                shipLeft < icebergRight &&
-                shipRight > icebergLeft
+        const shipLeft = this.ship.positionX;
+        const shipRight = this.ship.positionX + this.ship.width;
+        const shipTop = this.ship.positionY + this.ship.height;
+        const shipBottom = this.ship.positionY;
+
+        const icebergLeft = iceberg.positionX;
+        const icebergRight = iceberg.positionX + iceberg.size;
+        const icebergTop = iceberg.positionY + iceberg.size;
+        const icebergBottom = iceberg.positionY;
+
+        if (
+            shipLeft < icebergRight &&
+            shipRight > icebergLeft &&
+            shipTop > icebergBottom &&
+            shipBottom < icebergTop
         ) {
             // Collision detected!
             location.href = "./gameover.html";
@@ -103,25 +133,27 @@ class Ship {
         parentElm.appendChild(this.domElement);
     }
     moveUp() {
-        if (this.positionY < 100 -this.height) {
-        this.positionY++;
-        this.domElement.style.bottom = this.positionY + "vh";
+        if (this.positionY < 100 - this.height) {
+            this.positionY++;
+            this.domElement.style.bottom = this.positionY + "vh";
         }
     }
     moveDown() {
         if (this.positionY > 0) {
-        this.positionY--;
-        this.domElement.style.bottom = this.positionY + "vh";
+            this.positionY--;
+            this.domElement.style.bottom = this.positionY + "vh";
         }
     }
 }
 
 class Icebergs {
-    constructor () {
-        this.size = 20;
+    constructor(isAccelerated) {
+        this.size = 10;
         this.positionX = 100;
         this.positionY = Math.floor(Math.random() * (100 - this.size + 1)); //
         this.domElement = null;
+
+        this.isAccelerated = isAccelerated;
 
         this.createDomElement();
         this.createTriangle();
@@ -141,19 +173,42 @@ class Icebergs {
     }
     createTriangle() {
         const triangle = document.createElement("div");
-        triangle.id = "triangle";
         triangle.style.width = "0";
         triangle.style.height = "0";
         triangle.style.borderLeft = this.size / 2 + "vw solid transparent";
         triangle.style.borderRight = this.size / 2 + "vw solid transparent";
-        triangle.style.borderBottom = this.size + "vh solid white";
+        triangle.style.borderBottom = this.size + "vh solid transparent";
+        triangle.style.position = "relative";
+
+        // image positioning in centre of triangle
+        const centerX = this.size / 2;
+        const centerY = this.size;
+
+        // add the iceberg image
+        const icebergImage = document.createElement("img");
+        icebergImage.src = "./images/iceberg.png";
+        icebergImage.style.width = this.size + "vw";
+        icebergImage.style.height = this.size + "vh";
+        icebergImage.style.position = "absolute";
+        icebergImage.style.left = `calc(50% - ${centerX}vw)`;
+        icebergImage.style.bottom = `calc(100% - ${centerY}vh)`;
+
+        // apply acceleration if it's currently active
+        const speed = this.isAccelerated ? 4 : 2;
+        this.positionX -= speed;
+
+        // append image to triangle
+        triangle.appendChild(icebergImage);
 
         // append triangle to iceberg div 
         this.domElement.appendChild(triangle);
     }
     moveLeft() {
-        this.positionX -= 2;
-        this.domElement.style.left= this.positionX + "vw";
+        if (this.positionX > 0 - this.size) {
+        const speed = this.isAccelerated ? 4 : 2;
+        this.positionX -= speed;
+        this.domElement.style.left = this.positionX + "vw";
+        }
     }
 }
 
